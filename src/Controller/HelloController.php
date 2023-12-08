1ad907bb66d45b1c8498ce56631c80a9a6d52bf7
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-12-08 03:48:58 +0300

    introducing twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = [
         'mangoes',
@@ -18,13 +20,19 @@ class HelloController
     #[Route('/{limit?3}', name: 'app_index')]
     public function index(int $limit): Response
     {
-        return new Response(implode(' ', array_slice($this->messages, 0, $limit)));
+        return $this->render(
+            'index.html.twig',
+            ['messages' => implode(' ', array_slice($this->messages, 0, $limit))]
+        );
     }
 
     #[Route('/messages/{id}', name: 'app_show', requirements: ['id' => Requirement::DIGITS])]
     public function showOne(int $id): Response
     {
-        return new Response($this->messages[$id]);
+        return $this->render(
+            'show.html.twig',
+            ['message' => $this->messages[$id]]
+        );
     }
 
 }
